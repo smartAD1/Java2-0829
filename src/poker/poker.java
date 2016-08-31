d78e4f9a8eac73a6621582e9d75820b9c4b3c803
smartAD1 <gb4690237@gmail.com>
2016-08-31 09:56:19 +0800

    簡易型撲克牌 發牌

---
+++ src/poker/poker.java
@@ -0,0 +1,47 @@
+package poker;
+
+import java.util.Arrays;
+
+public class poker {
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		// 洗牌
+		int[] poker = new int[52];
+		for (int i = 0; i < poker.length; i++) {
+			int temp;
+
+			// 檢查機制
+			boolean isRepeat;
+			do {
+				temp = (int) (Math.random() * 52);
+				isRepeat = false;
+				for (int j = 0; j < i; j++) {
+					if (temp == poker[j]) {
+						isRepeat = true;
+						break;
+					}
+				}
+			} while (isRepeat);
+			poker[i] = temp;
+		}
+		// 發牌動作
+		int[][] player = new int[4][13];
+		for (int i = 0; i < poker.length; i++) {
+			player[i % 4][i / 4] = poker[i];
+		}
+		// 攤牌
+		String[] suit ={"黑桃","紅心","方塊","梅花"};
+		String[] value ={"A","2","3","4","5","6","7","8","9","10"
+				,"J","Q","K"};
+		
+		for(int[] cards:player){
+			Arrays.sort(cards);
+			for(int card:cards){
+				System.out.print(suit[card/13] + value[card%13] +"  ");
+			}
+			System.out.println();
+		}
+	}
+}
+
